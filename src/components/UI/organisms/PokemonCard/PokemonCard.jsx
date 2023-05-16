322bffb8c91ef7bdec804c9d1b08e9e5522af9bd
armendariz4 <javierarmendariz1.com>
2023-05-16 21:38:26 +0200

    Coments and imagenes changes

--- src/components/UI/organisms/PokemonCard/PokemonCard.jsx
+++ src/components/UI/organisms/PokemonCard/PokemonCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./styles.css";
-import { ReactComponent as WeightIcon } from "../../../../assets/images/icons/weight.svg";
-import { ReactComponent as HeightIcon } from "../../../../assets/images/icons/height.svg";
-import { AboutTitle } from "../../atoms/AboutTitle";
-import { ChipGroup } from "../../molecules/ChipGroup";
+import { ReactComponent as WeightIcon } from "../../../../assets/images/icons/weight.svg"; // Importo una imagen metiendola dentro de un componente llamado "WeightIcon", con la ayuda de la herramienta "ReactComponent"
+import { ReactComponent as HeightIcon } from "../../../../assets/images/icons/height.svg"; // Importo una imagen metiendola dentro de un componente llamado "HeightIcon", con la ayuda de la herramienta "ReactComponent"
+import { AboutTitle } from "../../atoms/AboutTitle"; // Importo la función AboutTitle para usarlo mas tarde en el código
+import { ChipGroup } from "../../molecules/ChipGroup"; // Importo la función ChipGroup para uarla mas tarde en el código
 
+//___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+// DIEGO SI LEES ESTO, ESTOY MODIFICANDO EL CÓDIGO, ACEPTAME POR DISCORD PARA HABLAR Y NO PISARNOS TRABAJO AL PUSHEAR
+// LO UNICO QUE VOY A HACER ES COMENTAR TODO E INTENTAR QUE APAREZCAN LAS IMAGENES
+
+// Esto es una unica función, que nos creará la carta entera, en función del objeto pokemon que le llega como entrada
 const PokemonCard = ({ pokemon }) => {
-  const mainType = pokemon.types[0];
-  const styles = `card type-${mainType}`;
+  const mainType = pokemon.types[0]; // Del atributo type del objeto, pokemon, se escoge el primer elemento y ese será el tipo principal de la carta
+  const styles = `card type-${mainType}`; // mete en "styles", los estilo que tiene la clase "class" + la clase "typeX". Siendo X, el tipo principal, que se obtiene en la linea de código anterior.
 
+  // De aqui para abajo, será lo que devuelva la función PokemonCard
   return (
     <article className={styles}>
       <div className="card-header">
@@ -16,7 +23,11 @@ const PokemonCard = ({ pokemon }) => {
         <span className="pokemon-number">#{pokemon.id}</span>
       </div>
       <div className="pokemon-attributes">
-        <img src={pokemon.src} alt="" className="poke-image"></img>
+        <img
+          src={"../../../../assets/images/pokemons/bulbasaur.svg"}
+          alt=""
+          className="poke-image"
+        ></img>
         <ChipGroup types={pokemon.types} />
         <AboutTitle type={mainType} />
         <div className="pokemon-data">
